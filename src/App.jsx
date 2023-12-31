546f521cf0205b936ea736a8cba7c321a72b8cec
px1412 <avinashmehta2016fzk@gmail.com>
2023-12-31 12:53:19 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center font-bold text-5xl text-red-600 bg-slate-800 w-screen h-screen">
+      App
+    </div>
+  );
+};
+
+export default App;
